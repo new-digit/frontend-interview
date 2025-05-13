62c81dbcd15141758e39f87c1f498c4eff11f7f7
smallgag <smallgag@gitlab.com>
2025-05-13 22:30:34 +0800

    feat: add filtering functionality to account data based on search string in filterAccountData utility

--- sheet/app/utils/filterAccountData.ts
+++ sheet/app/utils/filterAccountData.ts
@@ -1,5 +1,6 @@
 import { AccountData } from '../api/mock';
 
+// 透過搜尋字串，過濾帳號資料
 export function filterAccountData(data: AccountData[], search?: string): AccountData[] {
   if (!search) return data;
   const lower = search.toLowerCase();
